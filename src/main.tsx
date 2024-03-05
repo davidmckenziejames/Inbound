98acf87e57477be87bce425defe4da3eefb31a33
David McKenzie-James <142780219+sagemmj@users.noreply.github.com>
2024-03-05 21:06:14 +0000

    style: Remove unused imports and files from the project

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { loadStripe } from "@stripe/stripe-js";
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from "@stripe/react-stripe-js";
 import "@fontsource/inter/300.css";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
